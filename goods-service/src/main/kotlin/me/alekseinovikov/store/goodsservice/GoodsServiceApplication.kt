a867039fb59808411fba603de55dd87e86f9ebb5
Aleksei Novikov <novikov.aleksei.92@gmail.com>
2023-02-10 15:35:41 +0200

    Initial commit

---
+++ goods-service/src/main/kotlin/me/alekseinovikov/store/goodsservice/GoodsServiceApplication.kt
@@ -0,0 +1,11 @@
+package me.alekseinovikov.store.goodsservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GoodsServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<GoodsServiceApplication>(*args)
+}
